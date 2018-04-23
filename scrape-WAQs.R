7a9bd223876c1612331be4b37a9f7130271af4db
jackgovier <36526665+jackgovier@users.noreply.github.com>
2018-04-23 05:49:56 +0100

    Notes added

--- scrape-WAQs.R
+++ scrape-WAQs.R
@@ -1,7 +1,6 @@
 library(rvest)
 library(stringr)
 library(dplyr)
-library(curl)
 
 # Create initial dataframe
 df1 <- readRDS("Data/WQ.RDS")
@@ -16,11 +15,13 @@ wqn <- read_html("http://record.assembly.wales/Search/?type=7")
 waqn <- html_nodes(wqn, ".title")
 max <- as.integer(max(str_replace(html_text(waqn), "Written Question - WAQ", "")))
 
+# For loop to scrape information from Questions since table last updated
 
 for(i in min:max){
-  #Creates URL matching 
+  # Creates URL matching WAQ number
   wq <- read_html(paste0("http://record.assembly.wales/WrittenQuestion/",i))
   
+  # Find elements with useful information
   name <- html_nodes(wq, ".name")
   title <- html_nodes(wq, ".title")
   withdraw <- html_nodes(wq, ".withdrawn")
@@ -28,11 +29,14 @@ for(i in min:max){
   text <- html_nodes(wq, "p")
   welsh <- html_nodes(wq, ".welsh")
   
-  
+  # Retrieve information - removing whitespace before and after it
   amname <- str_trim(html_text(name))
   wqtitle <- str_trim(html_text(title[1]))
+  
+  # This tag does not exist where the Question was not withdrawn, so is populated by a check to see if it exists
   withdrawn <- !identical(html_text(withdraw),character(0))
   
+  # Unanswered/Withdrawn questions will frequently not have a 2nd node to query - this throws up an error if not accounted for
   if(length(title)>1){
     answeredby <- str_replace(str_trim(html_text(title[2])), "Answered by ","")
   } else {
@@ -56,16 +60,17 @@ for(i in min:max){
   
   welshyn <- !identical(html_text(welsh),character(0))
   
+  # Formatting collected data as a tibble
   df2 <- data_frame("question_no" = wqtitle, "name" = amname, "tabled_dt" = tabled,
                     "response_name" = answeredby, "response_dt" = answered, "question" = qtext,
                     "answer" = atext, "welsh" = welshyn, "withdrawn" = withdrawn, "number" = i)
   
+  # Appending to our existing tibble
   df1 <- bind_rows(df1, df2)
 }
 
 # For the time being, best to manually remove questions after the point at which multiple "Yet to be answered" responses slip in
 # Will at a later date add functionality that also removes and re-queries any WAQ where answer is ~ "To be answered by"
-
 df1 <- subset(df1,number < 76294)
 
 #Save output
